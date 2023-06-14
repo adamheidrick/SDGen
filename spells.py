5607487d7e63e4e7e3a24fa7dfc159ff2b56ae2f
adamheidrick <adamheidrick@gmail.com>
2023-06-14 09:37:41 +0200

    Filling in Wizard Spell details for all tiers.

--- spells.py
+++ spells.py
@@ -2,14 +2,17 @@ Priest_Spells = {"Light": ["Duration: 1 Hour Real Time",
                            "Range: Close",
                            "Details: One object you touch glows with brith, heat-less light, "
                            "illuminating out to a near distance for 1 hour of real time."],
+
                  "Cure Wounds": ["Duration: Instant",
                                  "Range: Close",
                                  "Details: Your touch restores ebbing life. Roll a number of d6s equal to  1+ "
                                  "half your level (rounded down). Once target you touch regains that many hit points."],
+
                  "Holy Weapon": ["Duration: 5 rounds",
                                  "Range: Close",
                                  "Details: One weapon you touch is imbued with a sacred blessing. The weapon becomes"
                                  "magical and has +1 to attack and damage rolls for the duration."],
+
                  "Protection from Evil": ["Duration: Focus",
                                           "Range: Close",
                                           "Details: For the spell's duration, chaotic beings have disadvantage on "
@@ -17,10 +20,12 @@ Priest_Spells = {"Light": ["Duration: 1 Hour Real Time",
                                           "beings also can't possess, compel, or beguile  it. When cast on an already"
                                           "possessed target, the possessing entity makes a CHA check vs. the last"
                                           "spellcasting check. On a failure, the entity is expelled."],
+
                  "Shield of Faith": ["Duration: 5 Rounds",
                                      "Range: Self",
                                      "Details: A protective fore wrought of your holy conviction surrounds you. You "
                                      "gain a +2 bonus to your armor class for the duration."],
+
                  "Turn Undead": ["Duration: Instant",
                                  "Range: Near",
                                  "Details: You rebuke undead creatures, forcing them to flee. You must present"
@@ -35,45 +40,57 @@ Tier_1_Spells = {
               "Details: You touch one object, such as a door threshold, setting a magical alarm on it. "
               "If any creature you do not designate while casting the spell touches or crosses past the "
               "object, a magical bell sounds in your head."],
+
     "Burning Hands": ["Duration: Instant",
                       "Range: Close",
                       "Details: You spread your fingers with thumbs touching, unleashing a circle of flame that "
                       "roars out to a close area around where you stand. Creatures within the area of effect "
                       "take 1d6 damage, and flammable objects catch fire."],
+
     "Charm Person": ["Duration: 1d8 days",
                      "Range: Near",
                      "Details: You magically beguile one humanoid of level 2 or less within near range, who views you "
                      "as a close friend for the duration. The spell ends if you or your allies do anything the target "
                      "notices to hurt it. The target knows you magically enchanted it after the spell end"],
+
     "Detect Magic": ["Duration: Focus",
                      "Range: Near",
                      "Details: You can sense the presence of magic within near range for the spell's duration. "
                      "If you focus for two rounds, you discern its general properties. Full barriers "
                      "block this spell."],
+
     "Feather Fall": ["Duration: Instant",
                      "Range: Self",
                      "Details: You may make an attempt to cast this spell when you fall. Your rate of descent slows so "
                      "that you land safely on your feet."],
-    "Floating Disc": ["Duration:",
-                      "Range:",
-                      "Details: "],
+
+    "Floating Disc": ["Duration: 5 Rounds",
+                      "Range: Near",
+                      "Details: You create a floating, circular disk of force with a concave center. It can carry up "
+                      "to 20 gear slots. It hovers at waist level and automatically stays within near of you. "
+                      "It can’t cross over dropoffs or pits taller than a human."],
+
     "Hold Portal": ["Duration: 10 rounds",
                     "Range: Near",
                     "Details: You create a floating, circular disk of force with a concave center. It can carry up to "
                     "20 gear slots. It hovers at waist level and automatically stays within near of you. It can’t "
                     "cross over dropoffs or pits taller than a human."],
+
     "Light": ["Duration: 1 hour real time",
               "Range: Close",
               "Details: One object you touch glows with bright, heatless light, illuminating out to a near distance "
               "for 1 hour of real time."],
+
     "Mage Armor": ["Duration: 10 rounds",
                    "Range: Self",
                    "Details: An invisible layer of magical force protects your vitals. Your armor class becomes 14 "
                    "(18 on a critical spellcasting check) for the spell’s duration."],
+
     "Magic Missile": ["Duration: Instant",
                       "Range: Far",
                       "Details: You have advantage on your check to cast this spell. A glowing bolt of force streaks "
                       "from your open hand, dealing 1d4 damage to one target."],
+
     "Protection from Evil": ["Duration: Focus",
                              "Range: Close",
                              "Details: For the spell’s duration, chaotic beings have disadvantage on attack rolls "
@@ -81,6 +98,7 @@ Tier_1_Spells = {
                              "compel, or beguile it. When cast on an alread ypossessed target, the possessing entity "
                              "makes a CHA check vs. the last spellcasting check. On a failure, the "
                              "entity is expelled."],
+
     "Sleep": ["Duration: Instant",
               "Range: Near",
               "Details: You weave a lulling spell that fills a near-sized cube extending from you. Choose a number of "
@@ -89,10 +107,29 @@ Tier_1_Spells = {
 }
 
 Tier_2_Spells = {
-    "Acid Arrow": ["Duration: ", "Range: ", "Details: "],
-    "Alter Self": ["Duration: ", "Range: ", "Details: "],
-    "Detect Thoughts": ["Duration: ", "Range: ", "Details: "],
-    "Fixed Object": ["Duration: ", "Range: ", "Details: "],
+    "Acid Arrow": ["Duration: Focus",
+                   "Range: Far",
+                   "Details: You conjure a corrosive bolt that hits one foe, dealing 1d6 damage a round. The bolt "
+                   "remains in the target for as long as you focus."],
+
+    "Alter Self": ["Duration: 5 Rounds",
+                   "Range: Self",
+                   "Details: You magically change your physical form, gaining one feature that modifies your existing "
+                   "anatomy. For example, you can grow functional gills on your neck or bear claws on your fingers. "
+                   "This spell can’t grow wings or limbs."],
+
+    "Detect Thoughts": ["Duration: Focus",
+                        "Range: Near",
+                        "Details: You peer into the mind of one creature you can see within range. Each round, you "
+                        "learn the target’s immediate thoughts. On its turn, the target makes a Wisdom check vs. your "
+                        "last spellcasting check. If the target succeeds, it notices your presence in its mind and the "
+                        "spell ends."],
+
+    "Fixed Object": ["Duration: 5 Rounds",
+                     "Range: Close",
+                     "Details: An object you touch that weighs no more than 5 pounds becomes fixed in its current "
+                     "location. It can support up to 5,000 pounds of weight for the duration of the spell."],
+
     "Hold Person": ["Duration: ", "Range: ", "Details: "],
     "Invisibility": ["Duration: ", "Range: ", "Details: "],
     "Knock": ["Duration: ", "Range: ", "Details: "],
@@ -104,12 +141,38 @@ Tier_2_Spells = {
 }
 
 Tier_3_Spells = {
-    "Animate Dead": ["Duration: ", "Range: ", "Details: "],
-    "Dispel Magic": ["Duration: ", "Range: ", "Details: "],
-    "Fabricate": ["Duration: ", "Range: ", "Details: "],
-    "Fireball": ["Duration: ", "Range: ", "Details: "],
-    "Fly": ["Duration: ", "Range: ", "Details: "],
-    "Gaseous Form": ["Duration: ", "Range: ", "Details: "],
+    "Animate Dead": ["Duration: 1 Day",
+                     "Range: Close",
+                     "Details: You touch one humanoid’s remains, and it rises as a zombie or skeleton under your "
+                     "control. The remains must have at least three limbs and its head intact. The undead creature "
+                     "acts on your turn. After 1 day, the creature collapses into grave dust."],
+
+    "Dispel Magic": ["Duration: Instant",
+                     "Range: Near",
+                     "Details: End one spell that affects one target you can see in range."],
+
+    "Fabricate": ["Duration: 10 Rounds",
+                  "Range: Near",
+                  "Details: This spell can't target creatures. You turn a tree-sized collection of raw materials into "
+                  "a finished work. For example, you convert a pile of bricks or rocks into a bridge. The finished "
+                  "work converts back to raw materials when the spell ends."],
+
+    "Fireball": ["Duration: Instant",
+                 "Range: Far",
+                 "Details: You hurl a small flame that erupts into a fiery blast. All creatures in a near-sized cube "
+                 "around where the flame lands take 4d6 damage."],
+
+    "Fly": ["Duration: 5 Rounds",
+            "Range: Self",
+            "Details: Your feet lift from the ground, and you take to the air like a hummingbird. You can fly near for "
+            "the spell's duration and are able to hover in place."],
+
+    "Gaseous Form": ["Duration: 10 Rounds",
+                     "Range: Self",
+                     "Details: You and your gear turn into a cloud of smoke for the spell's duration. You can fly and "
+                     "pass through any gap that smoke could. You can sense the terrain and any movement around you "
+                     "out to a near distance. You can't cast spells while in this form."],
+
     "Illusion": ["Duration: ", "Range: ", "Details: "],
     "Lightning Bolt": ["Duration: ", "Range: ", "Details: "],
     "Magic Circle": ["Duration: ", "Range: ", "Details: "],
@@ -118,12 +181,40 @@ Tier_3_Spells = {
     "Speak with Dead": ["Duration: ", "Range: ", "Details: "]
 }
 Tier_4_Spells = {
-    "Arcane Eye": ["Duration: ", "Range: ", "Details: "],
-    "Cloudkill": ["Duration: ", "Range: ", "Details: "],
-    "Confusion": ["Duration: ", "Range: ", "Details: "],
-    "Control Water": ["Duration: ", "Range: ", "Details: "],
-    "Dimension Door": ["Duration: ", "Range: ", "Details: "],
-    "Divination": ["Duration: ", "Range: ", "Details: "],
+    "Arcane Eye": ["Duration: Focus",
+                   "Range: Near",
+                   "Details: You conjure an invisible, grape-sized eye within range. You can see through the eye. It "
+                   "can see in the dark out to near range, fly near on your turn, and squeeze through openings as "
+                   "narrow as a keyhole."],
+
+    "Cloudkill": ["Duration: 5 Rounds",
+                  "Range: Far",
+                  "Details: A putrid cloud of yellow poison fills a near-sized cube within range. It spreads "
+                  "around corners. Creatures inside the cloud are blinded and take 2d6 damage at the beginning of "
+                  "their turns. A creature of LV 9 or less that ends its turn fully inside the cloud dies."],
+
+    "Confusion": ["Duration: Focus",
+                  "Range: Near",
+                  "Details: You mesmerize one creature you can see in range. The target can't take actions, and it "
+                  "moves in a random direction on its turn. If the target is LV 9+, it may make a WIS check vs. "
+                  "your last spellcasting check at the start of its turn to end the spell."],
+
+    "Control Water": ["Duration: Focus",
+                      "Range: Far",
+                      "Details: You move and shape water. You can cause a section of water up to 100 feet in width "
+                      "and depth to change shape, defy gravity, or flow in a different direction."],
+
+    "Dimension Door": ["Duration: Instant",
+                       "Range: Self",
+                       "Details: You teleport yourself and up to one other willing creature to any point you can see."],
+
+    "Divination": ["Duration: Instant",
+                   "Range: Self",
+                   "Details: You throw the divining bones or peer into the blackness between the stars, seeking a "
+                   "portent. You can ask the GM one yes or no question. The GM truthfully answers 'yes' or 'no.' If "
+                   "you cast this spell more than once in 24 hours, treat a failed spellcasting check for it as a "
+                   "critical failure, instead."],
+
     "Passwall": ["Duration: ", "Range: ", "Details: "],
     "Polymorph": ["Duration: ", "Range: ", "Details: "],
     "Resilient Sphere": ["Duration: ", "Range: ", "Details: "],
@@ -133,9 +224,25 @@ Tier_4_Spells = {
 }
 
 Tier_5_Spells = {
-    "Antimagic Shell": ["Duration: ", "Range: ", "Details: "],
-    "Create Undead": ["Duration: ", "Range: ", "Details: "],
-    "Disintegrate": ["Duration: ", "Range: ", "Details: "],
+    "Antimagic Shell": ["Duration: Focus",
+                        "Range: Self",
+                        "Details: An invisible, near-sized cube of null-magic appears centered on you. Within the "
+                        "cube, no spells can be cast. Magic items and spells have no effect in the zone, and no magic "
+                        "can enter. The cube moves with you. Spells such as dispel magic have no effect on it. Another "
+                        "antimagic shell does not affect this one."],
+
+    "Create Undead": ["Duration: 1 Day",
+                      "Range: Close",
+                      "Details: You conjure a vengeful undead creature to do your bidding. When you cast this spell, "
+                      "you choose to summon either a wight or wraith. It appears next to you and is under your "
+                      "control. The undead creature acts on your turn. After 1 day, it melts away into smoke."],
+
+    "Disintegrate": ["Duration: Instant",
+                     "Range: Far",
+                     "Details: A green ray shoots from your finger and turns a creature or object into ash. A target "
+                     "creature of LV 5 or less instantly dies. If it is LV 6+, it takes 3d8 damage, instead. A "
+                     "non-magical object up to the size of a large tree is destroyed."],
+
     "Hold Monster": ["Duration: ", "Range: ", "Details: "],
     "Plane Shift": ["Duration: ", "Range: ", "Details: "],
     "Power Word Kill": ["Duration: ", "Range: ", "Details: "],
