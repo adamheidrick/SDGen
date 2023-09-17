144390d24f4cc9ae0394c44ca87199550c02c72f
adamheidrick <adamheidrick@gmail.com>
2023-09-17 12:18:04 +0200

    Added Test file
    Error is with Armor Boost Talent.

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ import json
 
 from HeroCreator.hero_main import create_hero
 from HeroCreator.magic_item_gen import generate_magical_item
-
-hero = create_hero()
-hero.pop('talents')
-hero.pop('stats')
-cleaned = json.dumps(hero, indent=4)
-print(cleaned)
+for _ in range(10000):
+    hero = create_hero()
+    hero.pop('talents')
+    hero.pop('stats')
+    cleaned = json.dumps(hero, indent=4)
+    print(cleaned)
 
 # magic_weapon = generate_magical_item()
 # print(json.dumps(magic_weapon, indent=4))
