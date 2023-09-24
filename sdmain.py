ee2910e41ee1e16e77dd0b1da75080e442409430
adamheidrick <adamheidrick@gmail.com>
2023-09-24 09:00:16 +0200

    Different classes render different pages.

--- sdmain.py
+++ sdmain.py
@@ -13,7 +13,18 @@ def home():
 @app.route('/hero')
 def hero():
     gen_hero = create_hero()
-    return render_template('character.html', hero=gen_hero)
+    hero_class = gen_hero['hero_class']
+    match hero_class:
+        case 'Fighter':
+            return render_template('fighter.html', hero=gen_hero)
+        case 'Wizard':
+            return render_template('wizard.html', hero=gen_hero)
+        case 'Priest':
+            return render_template('priest.html', hero=gen_hero)
+        case 'Thief':
+            return render_template('thief.html', hero=gen_hero)
+        case _:
+            return render_template('error.html', hero=gen_hero)
 
 
 @app.route('/magicweapon')
